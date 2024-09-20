1e50cbe0e54afa6e1a4fe5de13bc91eab1955dd2
Adnan Hussain <136370255+AdnanH901@users.noreply.github.com>
2024-09-20 09:34:49 +0100

    Delete bnb directory

--- bnb/june_vacancies.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW june_vacancies AS
-SELECT listing_id, property_type, host_name, COUNT(available) AS "days_vacant"
-FROM listings
-JOIN availabilities ON availabilities.listing_id = listings.id
-WHERE "date" LIKE '2023-06-%'
-AND available = "TRUE"
-GROUP BY host_name
-limit 15;
-
